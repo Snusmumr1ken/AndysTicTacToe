f2e321df0f7044326ef7754db276db785fc5ba32
Andrii Nyvchyk <andrei.nivchik@gmail.com>
2019-08-10 17:11:52 +0300

    Update tic_tac_toe.rb

--- src/tic_tac_toe.rb
+++ src/tic_tac_toe.rb
@@ -5,7 +5,7 @@ require 'telegram/bot'
 require_relative 'init'
 require_relative 'pre_init'
 
-token = '810797047:AAHJ__oVXVBP6teg1i3hI4I48xsBcxZQexg'
+token = 'privacy our basis'
 
 Telegram::Bot::Client.run(token) do |bot|
   pre_init(bot)
